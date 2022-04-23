1d418bf15db601a68a75c4acfcc4996f9ee9ad83
jeffersonchen-tw <jeffersonchen-tw@users.noreply.github.com>
2022-04-23 21:34:38 +0800

    fix typo

--- src/main/UrlList.java
+++ src/main/UrlList.java
@@ -7,5 +7,5 @@ public interface UrlList {
    public static final String USER_DELETE_URL = "https://laundry-ordering.herokuapp.com/users/delete";
    public static final String USER_SET_CREDIT_URL = "https://laundry-ordering.herokuapp.com/users/set-credit";
    public static final String USER_CHANGE_PASSWORD_URL = "https://laundry-ordering.herokuapp.com/users/change-password";
-   public static final String GET_AVALIABLE_RESERVATION_TIME_URL = "https://laundry-ordering.herokuapp.com/reservations/get-avaliable-reservations";
+   public static final String GET_AVAILABLE_RESERVATION_TIME_URL = "https://laundry-ordering.herokuapp.com/reservations/get-available-reservations";
 }
