216979f25660a4df52c8b34dc40b637dacea1d21
jeffersonchen-tw <jeffersonchen-tw@users.noreply.github.com>
2022-04-23 21:36:05 +0800

    create individual directory for main tab

--- src/scenes/MainScene/MainSceneFactory.java
+++ src/scenes/MainScene/MainSceneFactory.java
@@ -3,6 +3,7 @@ package scenes.MainScene;
 import javafx.scene.control.Tab;
 import javafx.scene.control.TabPane;
 import javafx.stage.Stage;
+import scenes.MainScene.MainTab.MainTabFactory;
 
 public class MainSceneFactory {
 	public static TabPane create(Stage stage) {
