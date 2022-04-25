ec417b5de3e62859523a22a6cc726b03927e6677
Jefferson Chen <f1jefferson@yahoo.com.tw>
2022-04-25 23:04:21 +0800

    Parsing the return time from api to HH:mm format

--- src/scenes/MainScene/MainTabFactory.java
+++ src/scenes/MainScene/MainTabFactory.java
@@ -2,7 +2,10 @@ package scenes.MainScene;
 
 import java.time.LocalDate;
 import java.util.ArrayList;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
+import org.json.JSONArray;
 import org.json.JSONObject;
 
 import javafx.collections.FXCollections;
@@ -24,15 +27,6 @@ import utils.AppData;
 import utils.GetRequest;
 
 public class MainTabFactory {
-	private static class ListCell extends HBox {
-		Label label;
-
-		public ListCell(String labelText) {
-			super();
-			label = new Label(labelText);
-			this.getChildren().add(label);
-		}
-	}
 
 	final static Callback<DatePicker, DateCell> dayCellFactory = new Callback<DatePicker, DateCell>() {
 		@Override
@@ -47,6 +41,16 @@ public class MainTabFactory {
 			};
 		}
 	};
+	
+	private static String parsingJsonTimeString(String input) {
+		Pattern pattern = Pattern.compile("(\\S+)\\s+(\\S+)");
+		Matcher matcher = pattern.matcher(input);
+		while (matcher.find()) {
+			String timeString = matcher.group(2);
+			return timeString.substring(0, timeString.length() - 3);
+		}
+		return null;
+	}
 
 	public static Tab create() {
 		Tab tab = new Tab();
@@ -62,7 +66,11 @@ public class MainTabFactory {
 		mainVBox.setSpacing(15);
 		mainVBox.setPadding(new Insets(25, 0, 0, 0));
 		mainVBox.setAlignment(Pos.CENTER);
+		//
 		DatePicker datePicker = new DatePicker();
+		//
+		ObservableList<String> availableTimeList = FXCollections.observableArrayList();
+		ListView<String> listView = new ListView<>(availableTimeList);
 		datePicker.valueProperty().addListener((observable, oldDate, newDate) -> {
 			// do something when value of DatePicker is changed
 			String selectedDateString = newDate.toString();
@@ -71,22 +79,19 @@ public class MainTabFactory {
 			request.sendRequest(urlString);
 			int responseCode = request.getResponseCode();
 			if (responseCode < 299) {
-				System.out.println(request.getContent());
 				JSONObject json = new JSONObject(request.getContent());
 				if (json.getInt("replyCode") > 0) {
-
+					JSONArray jsonTimeList = json.getJSONArray("availableTimeList");
+					for (int i=0; i<jsonTimeList.length(); i++) {
+						String timeString = jsonTimeList.getString(i);
+						availableTimeList.add(parsingJsonTimeString(timeString));
+					}
 				}
 			}
 			request.closeConnection();
-		});
-		datePicker.setDayCellFactory(dayCellFactory);
-		ArrayList<ListCell> availableTimeBtnList = new ArrayList<>();
-		for (int i = 0; i < 12; i++) {
-			availableTimeBtnList.add(new ListCell("Item " + i));
 		}
-		ListView<ListCell> listView = new ListView<>();
-		ObservableList<ListCell> observableList = FXCollections.observableArrayList(availableTimeBtnList);
-		listView.setItems(observableList);
+		);
+		datePicker.setDayCellFactory(dayCellFactory);
 		mainVBox.getChildren().addAll(datePicker, listView);
 		vBox.getChildren().add(mainVBox);
 		tab.setContent(vBox);
