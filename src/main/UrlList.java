ce989a382e7706513bb7a30ad4edca68583b1a5f
Jefferson Chen <f1jefferson@yahoo.com.tw>
2022-04-30 23:51:16 +0800

    get all reservations url added

--- src/main/UrlList.java
+++ src/main/UrlList.java
@@ -9,4 +9,5 @@ public interface UrlList {
    public static final String USER_CHANGE_PASSWORD_URL = "https://laundry-ordering.herokuapp.com/users/change-password";
    public static final String GET_AVAILABLE_RESERVATION_TIME_URL = "https://laundry-ordering.herokuapp.com/reservations/get-available-reservations";
    public static final String USER_RESERVE_URL = "https://laundry-ordering.herokuapp.com/users/reserve";
+   public static final String USER_GET_ALL_RESERVATIONS = "https://laundry-ordering.herokuapp.com/users/get-user-reservations";
 }
